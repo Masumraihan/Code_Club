b108d060bf25c1cfb375c92437b889b9f4985298
Masumraihan <mdmasumraihan1@gamil.com>
2023-07-03 22:41:30 +0600

    updated

--- src/components/DbFunctions/dbFunctions.js
+++ src/components/DbFunctions/dbFunctions.js
@@ -1,21 +1,20 @@
 import { toast } from "react-hot-toast";
 
 const addToLocal = (id) => {
-    const previousId = JSON.parse(localStorage.getItem("job-id"));
-    let newId = []
-    if (previousId) {
-        const isExist = previousId.find(pId => pId === id);
-        if (!isExist) {
-            newId = [...previousId,id]
-        } else{
-            toast("You already applied this job");
-            return;
-        }
+  const previousId = JSON.parse(localStorage.getItem("job-id"));
+  let newId = [];
+  if (previousId) {
+    const isExist = previousId.find((pId) => pId === id);
+    if (!isExist) {
+      newId = [...previousId, id];
+    } else {
+      toast("You already applied this job");
+      return;
     }
-    else{
-        newId.push(id)
-    }
-    localStorage.setItem("job-id", JSON.stringify(newId));
-}
+  } else {
+    newId.push(id);
+  }
+  localStorage.setItem("job-id", JSON.stringify(newId));
+};
 
-export {addToLocal}
+export { addToLocal };
